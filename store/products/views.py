0004e8eb76d09474ef500811fb8539f087f510ed
lev <levitworld@gmail.com>
2016-07-10 02:05:08 +0800

    display product

--- store/products/views.py
+++ store/products/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
-def index(request):
-    return render(request, 'products/index.html', {})
+def product_list(request):
+	product = Product.objects.all()
+	return render (request, 'products/index.html', {'product': product})
